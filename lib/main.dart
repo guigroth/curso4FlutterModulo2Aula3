2a9e9d1c8ba717f08bc61e64e6a0e64dd01e0074
Guilherme Augusto Groth <guilhermegroth@gmail.com>
2024-01-09 23:09:12 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+
+  JournalService journalService = JournalService();
+  journalService.register("Olá mundo!");
+  //journalService.get();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+    );
+  }
+}
